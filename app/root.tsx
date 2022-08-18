ddb66ec863af89d9b0f7b374885046da1803aa36
pardity <contact@stephenpardue.com>
2022-08-17 20:45:30 -0400

    lots of changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import styles from "./tailwind.css";
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Link,
@@ -11,35 +10,24 @@ import {
   useCatch,
 } from "@remix-run/react";
 import * as React from "react";
-
-import deleteMeRemixStyles from "~/styles/demos/remix.css";
 import globalStylesUrl from "~/styles/global.css";
-import darkStylesUrl from "~/styles/dark.css";
 
-/**
- * The `links` export is a function that returns an array of objects that map to
- * the attributes for an HTML `<link>` element. These will load `<link>` tags on
- * every route in the app, but individual routes can include their own links
- * that are automatically unloaded when a user navigates away from the route.
- *
- * https://remix.run/api/app#links
- */
+
 export const links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: globalStylesUrl },
-    {
-      rel: "stylesheet",
-      href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)",
-    },
-    { rel: "stylesheet", href: deleteMeRemixStyles },
-  ];
-};
+    {rel: 'stylesheet', href: globalStylesUrl }
+    ];
+  };
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  viewport: "width=device-width,initial-scale=1",
-});
+  export const meta: MetaFunction = () => {
+    const description = "A cool blog built with remix"
+    const keywords = "remix, react, js"
+
+    return {
+      description, keywords,
+    }
+  };
+
 
 /**
  * The root module's default export is a component that renders the current
@@ -50,7 +38,7 @@ export default function App() {
   return (
     <Document>
       <Layout>
-        <Outlet />
+          <Outlet />
       </Layout>
     </Document>
   );
@@ -66,15 +54,13 @@ function Document({
   return (
     <html lang="en">
       <head>
-        {title ? <title>{title}</title> : null}
-        <Meta />
         <Links />
+        <title>{title ? title : 'My Remix Blog' }</title> 
       </head>
       <body>
         {children}
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        {process.env.NODE_ENV === 'development' ?
+        <LiveReload /> : null}
       </body>
     </html>
   );
@@ -82,86 +68,21 @@ function Document({
 
 function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
-    <div className="remix-app">
-      <header className="remix-app__header">
-        <div className="container remix-app__header-content">
-          <nav aria-label="Main navigation" className="remix-app__header-nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <a href="https://remix.run/docs">Remix Docs</a>
-              </li>
-              <li>
-                <a href="https://github.com/remix-run/remix">GitHub</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div className="remix-app__main">
-        <div className="container remix-app__main-content">{children}</div>
-      </div>
-      <footer className="remix-app__footer">
-        <div className="container remix-app__footer-content">
-          <p>&copy; You!</p>
-        </div>
-      </footer>
+    <>
+    <nav className="navbar">
+      <Link to='/' className='logo'>
+        Remix
+      </Link>
+
+      <ul className="nav">
+        <li>
+            <Link to='/posts'>Posts</Link>
+        </li>
+      </ul>
+    </nav>
+    <div className="container">
+      {children}
     </div>
-  );
-}
-
-export function CatchBoundary() {
-  const caught = useCatch();
-
-  let message;
-  switch (caught.status) {
-    case 401:
-      message = (
-        <p>
-          Oops! Looks like you tried to visit a page that you do not have access
-          to.
-        </p>
-      );
-      break;
-    case 404:
-      message = (
-        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
-      );
-      break;
-
-    default:
-      throw new Error(caught.data || caught.statusText);
-  }
-
-  return (
-    <Document title={`${caught.status} ${caught.statusText}`}>
-      <Layout>
-        <h1>
-          {caught.status}: {caught.statusText}
-        </h1>
-        {message}
-      </Layout>
-    </Document>
-  );
-}
-
-export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
-  return (
-    <Document title="Error!">
-      <Layout>
-        <div>
-          <h1>There was an error</h1>
-          <p>{error.message}</p>
-          <hr />
-          <p>
-            Hey, developer, you should replace this with what you want your
-            users to see.
-          </p>
-        </div>
-      </Layout>
-    </Document>
-  );
+    </>
+  )
 }
--- app/routes/demos/correct.tsx
+++
@@ -1,3 +0,0 @@
-export default function NiceWork() {
-  return <h1 className="text-3xl font-bold underline">You got it right!</h1>;
-}
