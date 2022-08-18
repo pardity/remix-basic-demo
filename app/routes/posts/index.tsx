6d4ab415c5bedb368884614ea1a442db796d2508
pardity <contact@stephenpardue.com>
2022-08-17 22:05:04 -0400

    Update index.tsx

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,16 +1,15 @@
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 
-
-export const loader = () => {
+export const loader: LoaderFunction = async () => {
     const data = {
         posts: [
             {id: 1, title: 'Post 1', body: 'this is a test post'},
             {id: 2, title: 'Post 2', body: 'this is a test post'},
-            {id: 3, title: 'Post 3', body: 'this is a test post'},
+            {id: 3, title: 'Post 3', body: 'this is a test post'}
         ]
     }
-    return {}
+    return data
 }
 
 function PostItems() {
@@ -20,7 +19,7 @@ function PostItems() {
     <div>
       <h1>Posts</h1>
       <ul className="posts-list">
-      {posts?.map((post) => (
+      {posts.map((post) => (
             <li key={post.id}>
                 <Link to={post.id}>    
                     <h3>{post.title}</h3>
